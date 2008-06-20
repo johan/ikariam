a21a6af6bd7bd711dce2fe6312a1e55b16d24ff0
Johan Sundström <oyasumi@gmail.com>
2008-06-20 07:30:16 +0000

    Slingers may suck, but they still can't be level -1.
    git-svn-id: http://ecmanaut.googlecode.com/svn/trunk/sites/ikariam.org@657 c5938665-bd24-0410-9958-d597d88377d4

--- kronos-utils/kronos_utils.user.js
+++ kronos-utils/kronos_utils.user.js
@@ -789,8 +789,8 @@ function militaryAdvisorReportViewView() {
 function detailedCombatView() {
   function register(unit, i) {
     if (!unit.A || !unit.D) return;
-    var att = Math.floor((a[i] - unit.a) / unit.A);
-    var def = Math.floor((d[i] - unit.d) / unit.D);
+    var att = Math.max(0, Math.floor((a[i] - unit.a) / unit.A));
+    var def = Math.max(0, Math.floor((d[i] - unit.d) / unit.D));
     units[unit.id] = n[i];
     levels[unit.id] = { a: att, d: def };
     att = <img alt={"["+ att +"]"} src={ gfx.sword(att) }/>;
