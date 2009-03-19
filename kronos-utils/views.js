a593bf2f4475c3c8f3c9808c06b7a0b261d58b9b
oyasumi <oyasumi@c5938665-bd24-0410-9958-d597d88377d4>
2009-03-19 20:27:18 +0000

    Imported remote icon into the Kronos code base.
    git-svn-id: http://ecmanaut.googlecode.com/svn/trunk/sites/ikariam.org@856 c5938665-bd24-0410-9958-d597d88377d4

--- kronos-utils/views.js
+++ kronos-utils/views.js
@@ -44,7 +44,8 @@ function cityView() {
   }
   projectCompletion("cityCountdown", null, '../preceding-sibling::a');
   levelBat();
-  cssToggler("buildinglevels", false, "http://i297.photobucket.com/albums/mm209/apocalypse33/Avatar/Ava51.png", "li > div.rounded { display: none; }");
+  cssToggler("buildinglevels", false, base + "building-levels.png.png",
+	     "li > div.rounded { display: none; }");
   findPlayers();
 }
 
